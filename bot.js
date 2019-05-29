6fe87affaf4958faf59098a32511d05f3c73df04
Abdellhadi12 <44175418+Abdellhadi12@users.noreply.github.com>
2019-05-28 22:14:52 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -354,7 +354,7 @@ if (!rank) return message.reply(' ')
 });
 
 
-client.login(process.env.TOKEN2);
+client.login(process.env.TOKEN);
 client2.login(process.env.TOKEN2);
 client3.login(process.env.TOKEN3);
 client4.login(process.env.TOKEN4);
